1455dfa5ff2d41fe4588eb74c7ab0fa3be4e590e
Stepan Korobov <stepa.korobov@gmail.com>
2024-09-15 22:26:33 +0400

    added annotation type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from dotenv import dotenv_values
+from typing import Dict
 
 from cloud import Cloud
 from synchronization import Synchronization
@@ -8,10 +9,9 @@ from synchronization import Synchronization
 
 class Main:
     @classmethod
-    def __get_env(cls):
+    def __get_env(cls) -> Dict:
         """Метод для загрузки переменных окружения"""
         config = dotenv_values(".env")
-        print(type(config))
 
         return config
 
@@ -21,19 +21,30 @@ class Main:
 
     @classmethod
     def start(cls):
+        # Получаем переменные окружения
         config = cls.__get_env()
 
+        # Путь до локальной папки с файлами
         path_sync_folder: str = config.get("PATH_SYNC_FOLDER")
+        # Путь до папки в облаке
         path_cloud_folder: str = config.get("CLOUD_STORAGE_FOLDER")
+        # Токен
         token: str = config.get('TOKEN')
+        # Период синхронизации
         sync_period: int = int(config.get("SYNC_PERIOD"))
+        # Путь к файлу с логами
         path_log_file: str = config.get("PATH_LOG_FILE")
 
+        # Создаём экземпляр класса для работы с облаком
         cloud = Cloud(token=token, dir_name=path_cloud_folder)
+        # Создаём экземпляр класса для работы с синхронизацией
         synchronization = Synchronization(dir_path=path_sync_folder, cloud=cloud)
 
+        # запускаем бесконечный цикл
         while True:
+            # Синхронизируем файлы
             synchronization.synchronize()
+            # Засыпаем на установленный период
             time.sleep(sync_period)
 
 
