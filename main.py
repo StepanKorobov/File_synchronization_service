7ced47ded8217595e6788347d494578794d053f4
Stepan Korobov <stepa.korobov@gmail.com>
2024-09-15 21:19:16 +0400

    added class Main and config

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
+from dotenv import dotenv_values
 
+from cloud import Cloud
 from synchronization import Synchronization
+
+class Main:
+    @classmethod
+    def __get_env(cls):
+        """Метод для загрузки переменных окружения"""
+        config = dotenv_values(".env")
+
+        return config
+
+    @classmethod
+    def start(cls):
+        config = cls.__get_env()
+
 
 
 if __name__ == "__main__":
-    pass
-    # synchronize = Synchronization()
+    # main()
+    Main.get_env()
 
