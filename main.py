11a01ddd98eda461b3bf6705513171f44d356b65
Stepan Korobov <stepa.korobov@gmail.com>
2024-09-15 21:50:44 +0400

    added annotation type

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import os
+import time
 from dotenv import dotenv_values
 
 from cloud import Cloud
 from synchronization import Synchronization
+
 
 class Main:
     @classmethod
     def __get_env(cls):
         """Метод для загрузки переменных окружения"""
         config = dotenv_values(".env")
+        print(type(config))
 
         return config
+
+    @classmethod
+    def __logging_settings(cls, path_log_file: str):
+        pass
 
     @classmethod
     def start(cls):
         config = cls.__get_env()
 
+        path_sync_folder: str = config.get("PATH_SYNC_FOLDER")
+        path_cloud_folder: str = config.get("CLOUD_STORAGE_FOLDER")
+        token: str = config.get('TOKEN')
+        sync_period: int = int(config.get("SYNC_PERIOD"))
+        path_log_file: str = config.get("PATH_LOG_FILE")
+
+        cloud = Cloud(token=token, dir_name=path_cloud_folder)
+        synchronization = Synchronization(dir_path=path_sync_folder, cloud=cloud)
+
+        while True:
+            synchronization.synchronize()
+            time.sleep(sync_period)
 
 
 if __name__ == "__main__":
-    # main()
-    Main.get_env()
-
+    Main.start()
--- synchronization.py
+++ synchronization.py
@@ -5,25 +5,40 @@ from cloud import Cloud
 
 
 class Synchronization:
+    """
+    Класс для синхронизации файлов
+    """
     def __init__(self, dir_path: str, cloud: Cloud):
         self.__dir_path: str = dir_path
         self.__cloud: Cloud = cloud
 
 
     def __get_all_files(self) -> Dict[str, str]:
+        """
+        Метод для получения всех файлов из папки на локальном диске
+
+        :return: словарь с именами файлов и датой обновления
+        :rtype: Dict[str, str]
+        """
+        # Словарь с названием файлов и датой последнего обновления
         files: Dict = dict()
 
-        path_to_dir = os.path.abspath(self.__dir_path)
+        # Путь к папке с файлами
+        path_to_dir: str = os.path.abspath(self.__dir_path)
 
+        # В цикле проходимся по папке, нам нужны только файлы
         for i_file in os.listdir(path_to_dir):
-            file_path = os.path.abspath(os.path.join(path_to_dir, i_file))
+            # Путь до файла/папки
+            file_path: str = os.path.abspath(os.path.join(path_to_dir, i_file))
 
+            # Если файл, то сохраняем путь и время в словаре
             if os.path.isfile(file_path):
-                file_name = os.path.basename(file_path)
-                file_date = os.path.getmtime(file_path)
+                file_name: str = os.path.basename(file_path)
+                file_date: float = os.path.getmtime(file_path)
 
-                files[file_name] = file_date
+                files[file_name]: float = file_date
 
+        # возвращаем словарь с файлами
         return files
 
     def synchronize(self):
